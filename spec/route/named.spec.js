1a0a0f551c96bf04321af9bb5855fa36ff0303ec
Eric Kidder <kageneko@evilkitten.org>
2013-06-27 16:10:56 -0400

    Updated named route tests

--- spec/route/named.spec.js
+++ spec/route/named.spec.js
@@ -16,10 +16,58 @@ describe('Named routes:', function() {
 		expect(namedRoute.matches('')).toBeNull();
 	});
 	
-	it('Testing multiple parameter route', function() {
+	it('Testing multiple optional parameter route', function() {
 		var namedRoute = new route.Route.Named('/:entity/:id', 'controller');
 		expect(namedRoute.matches('/entity/id')).toEqual({input: '/entity/id', parameterCount: 2, parameters: {entity: 'entity', id: 'id'}});
 		expect(namedRoute.matches('/entity/')).toEqual({input: '/entity/', parameterCount: 2, parameters: {entity: 'entity', id: ''}});
+		expect(namedRoute.matches('/entity')).toBeNull();
+	});
+
+	it('Testing multiple optional parameter route with static component', function() {
+		var namedRoute = new route.Route.Named('/:entity/id/:id', 'controller');
+		expect(namedRoute.matches('/entity/id/1')).toEqual({input: '/entity/id/1', parameterCount: 2, parameters: {entity: 'entity', id: '1'}});
+		expect(namedRoute.matches('//id/')).toEqual({input: '//id/', parameterCount: 2, parameters: {entity: '', id: ''}});
+		expect(namedRoute.matches('/entity/id')).toBeNull();
+		expect(namedRoute.matches('/entity')).toBeNull();
+	});
+	
+	it('Testing single required parameter route', function() {
+		var namedRoute = new route.Route.Named('/=id', 'controller');
+		expect(namedRoute.matches('/')).toBeNull();
+		expect(namedRoute.matches('/test')).toEqual({input: '/test', parameterCount: 1, parameters: {id: 'test'}});
+		expect(namedRoute.matches('/test/id')).toEqual({input: '/test/id', parameterCount: 1, parameters: {id: 'test/id'}});
+		expect(namedRoute.matches('')).toBeNull();
+	});
+	
+	it('Testing multiple required parameter route', function() {
+		var namedRoute = new route.Route.Named('/=entity/=id', 'controller');
+		expect(namedRoute.matches('/entity/id')).toEqual({input: '/entity/id', parameterCount: 2, parameters: {entity: 'entity', id: 'id'}});
+		expect(namedRoute.matches('/entity/')).toBeNull();
+		expect(namedRoute.matches('/entity')).toBeNull();
+	});
+
+	it('Testing multiple required parameter route with static component', function() {
+		var namedRoute = new route.Route.Named('/=entity/id/=id', 'controller');
+		expect(namedRoute.matches('/entity/id/1')).toEqual({input: '/entity/id/1', parameterCount: 2, parameters: {entity: 'entity', id: '1'}});
+		expect(namedRoute.matches('//id/')).toBeNull();
+		expect(namedRoute.matches('/entity/id')).toBeNull();
+		expect(namedRoute.matches('/entity')).toBeNull();
+	});
+	
+	it('Testing multiple mixed parameter route', function() {
+		var namedRoute = new route.Route.Named('/:entity/=id', 'controller');
+		expect(namedRoute.matches('/entity/id')).toEqual({input: '/entity/id', parameterCount: 2, parameters: {entity: 'entity', id: 'id'}});
+		expect(namedRoute.matches('//id')).toEqual({input: '//id', parameterCount: 2, parameters: {entity: '', id: 'id'}});
+		expect(namedRoute.matches('/entity/')).toBeNull();
+		expect(namedRoute.matches('/entity')).toBeNull();
+	});
+
+	it('Testing multiple mixed parameter route with static component', function() {
+		var namedRoute = new route.Route.Named('/=entity/id/:id', 'controller');
+		expect(namedRoute.matches('/entity/id/1')).toEqual({input: '/entity/id/1', parameterCount: 2, parameters: {entity: 'entity', id: '1'}});
+		expect(namedRoute.matches('/entity/id/')).toEqual({input: '/entity/id/', parameterCount: 2, parameters: {entity: 'entity', id: ''}});
+		expect(namedRoute.matches('//id/')).toBeNull();
+		expect(namedRoute.matches('/entity/id')).toBeNull();
 		expect(namedRoute.matches('/entity')).toBeNull();
 	});
 });
